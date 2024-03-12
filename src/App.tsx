dde32bad20065b5c1936bb66e74907e3164cf4e0
spiralabhishek <abhishek.spiral@gmail.com>
2024-03-12 13:27:56 +0530

    code changed

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          code changed 
+          Yogendra sinh zala
         </p>
         <a
           className="App-link"

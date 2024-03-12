0ff55d4110baa1ed0a409511b1d106239d7aa31e
spiralabhishek <abhishek.spiral@gmail.com>
2024-03-12 13:32:02 +0530

    code change df

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Yogendra sinh zala
+          ABhishek ANtala
         </p>
         <a
           className="App-link"

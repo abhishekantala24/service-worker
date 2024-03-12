9bcb879202fa1f111af0bc0fe03e4d94421d0fbb
spiralabhishek <abhishek.spiral@gmail.com>
2024-03-12 13:11:35 +0530

    code changed

--- src/App.js
+++
@@ -1,9 +0,0 @@
-// import the page container
-import PageContainer from "./components/PageContainer";
-
-// App component holds the page container for the application
-function App() {
-  return <PageContainer />;
-}
-
-export default App;

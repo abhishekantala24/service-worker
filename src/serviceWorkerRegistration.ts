59650f244dd9761df9e06da0fca0ee312aaff835
spiralabhishek <abhishek.spiral@gmail.com>
2024-04-24 11:34:43 +0530

    update

--- src/serviceWorkerRegistration.ts
+++ src/serviceWorkerRegistration.ts
@@ -68,7 +68,7 @@ function registerValidSW(swUrl: string, config?: Config) {
       setInterval(() => {
         registration.update();
         console.debug("Checked for update...");
-      }, 1000 * 60 * 1);
+      }, 15000);
       registration.onupdatefound = () => {
         const installingWorker = registration.installing;
         if (installingWorker == null) {
